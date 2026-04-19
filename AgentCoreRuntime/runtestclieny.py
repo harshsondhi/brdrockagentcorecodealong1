331c358f7b1a920768e1dd0b3fc859fffbe4c17f
harsh sondhi <harshsondhi2008@gmail.com>
2026-04-19 10:24:44 -0700

    Update runtestclieny.py

--- AgentCoreRuntime/runtestclieny.py
+++ AgentCoreRuntime/runtestclieny.py
@@ -5,7 +5,7 @@ client = boto3.client('bedrock-agentcore', region_name='us-west-2')
 payload = json.dumps({"prompt": "Data, tell me who created you"})
 
 response = client.invoke_agent_runtime(
-    agentRuntimeArn='arn:aws:bedrock-agentcore:us-west-2:615345689008:runtime/dataAgentCore-d836nU27p0',
+    agentRuntimeArn='arn:aws:bedrock-agentcore:us-west-2:615345689',
     runtimeSessionId='m7Qp9Xv2Lk8Rz4Nw1Tj6Hs3Bd0Fy5Ua7EcP', # Must be 33+ char. Every new SessionId will create a new MicroVM
     payload=payload,
     qualifier="DEFAULT" # This is Optional. When the field is not provided, Runtime will use DEFAULT endpoint
